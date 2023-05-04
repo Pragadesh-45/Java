b8a43f11d02b612b224bca305ef6694ac7714e23
Pragadesh-45 <temporaryg7904@gmail.com>
2023-05-04 16:24:45 +0530

    x

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
-  public stativ void main (String[] args) { 
-    System.out.println("Hello World!");
-  }
-}
+	public static void main (String args[]) {
+		System.out.println("Hello World!");
+		}
+	}
