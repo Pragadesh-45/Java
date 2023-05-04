2fa1cae5b6a48adef6758da060d4dc915b967448
Pragadesh-45 <temporaryg7904@gmail.com>
2023-05-04 20:24:45 +0530

    updated

---
+++ com/Pragadesh/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main (String args[]) {
+		//System.out.println("Hello World!");
+		//Scanner input = new Scanner(System.in);
+		//System.out.println(input.nextInt());
+		
+		int rollno = 64;
+		char letter = 'r';
+		float marks = 98.43f;
+		double largeDecimalNumbers = 1231243.1231;
+		long largeInteger = 12342351;
+		boolean check = true; //false;
+		
+		
+		
+		}
+	}
