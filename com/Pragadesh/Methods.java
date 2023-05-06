fd05f318562066a4c3ee496e84e3a0fed0dd5055
Pragadesh-45 <temporaryg7904@gmail.com>
2023-05-06 12:00:29 +0530

    Methods

---
+++ com/Pragadesh/Methods.java
@@ -0,0 +1,15 @@
+package com.Pragadesh;
+
+public class Methods {
+	public static void main(String args[]) {
+		add(12,13);
+		System.out.println("a is printed");
+	}
+
+	public static void add(int a, int b) {
+		// int a = 10;
+		// int b = 12;
+		int c = a+b;
+		System.out.println(c);
+	}
+}
