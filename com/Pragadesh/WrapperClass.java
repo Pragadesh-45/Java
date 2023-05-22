fae21a462916bcc295a305627769f25db2bd44ea
Pragadesh-45 <temporaryg7904@gmail.com>
2023-05-22 19:10:21 +0530

    updates

---
+++ com/Pragadesh/WrapperClass.java
@@ -0,0 +1,13 @@
+package com.Pragadesh;
+
+public class WrapperClass {
+	public static void main(String[] args) {
+		Boolean a = true;
+		Character b = '@';
+		Integer c = 123;
+		Double d = 3.14;
+		String e = "Pragadesh";
+
+
+	}
+}
