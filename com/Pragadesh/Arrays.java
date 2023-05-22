6db394f5efdcdfc0261dcb30caf42f63a442137d
Pragadesh-45 <temporaryg7904@gmail.com>
2023-05-22 18:55:03 +0530

    Arrays

--- com/Pragadesh/Arrays.java
+++ com/Pragadesh/Arrays.java
@@ -5,10 +5,23 @@ public class Arrays {
 	public static void main(String[] args) {
 		
 
-		String cars[] = {"Camaro", "Mustang", "Lancer"};
+		// String cars[] = {"Camaro", "Mustang", "Lancer"};
 
-		cars[0] = "Tesla";
+		// cars[0] = "Tesla";
 
-		System.out.println(cars[0]);
+		// System.out.println(cars[0]);
+
+
+		String[] cars = new String[3];
+
+		cars[0] = "Chervolet";
+		cars[1] = "Contesa";
+		cars[2] = "Mustang";
+
+		for(int i = 0; i<cars.length; i++) {
+			System.out.println(cars[i]);
+
+		}
+
 	}
 }
