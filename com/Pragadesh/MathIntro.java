feaf298a0c3a0b545a70da26b77182bc8448e292
Pragadesh-45 <temporaryg7904@gmail.com>
2023-05-22 13:12:48 +0530

    MathIntro

---
+++ com/Pragadesh/MathIntro.java
@@ -0,0 +1,11 @@
+package com.Pragadesh;
+
+public class MathIntro {
+	public static void main(String[] args) {
+
+		double x = 3.14;
+		double y = -10;
+
+		System.out.println(Math.max(x,y));;
+	}
+}
