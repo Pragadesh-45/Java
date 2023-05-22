51312b8da84ecf27badc392f5a32529177e64f84
Pragadesh-45 <temporaryg7904@gmail.com>
2023-05-22 13:00:56 +0530

    updated

---
+++ com/Pragadesh/Basic.java
@@ -0,0 +1,15 @@
+package com.Pragadesh;
+
+public class Basic{
+	public static void main (String[] args) {
+
+		int x = 123; //initialization
+		float y = 3.14f;
+		boolean z = true;
+		char symbol = '@';
+		String name = "Pragadesh";
+
+		// System.out.println("My number is"+symbol);
+		System.out.println("Hello " + name);
+		}
+} 
