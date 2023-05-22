feaa51c9d6695f69a935193eed5168261ee41719
Pragadesh-45 <temporaryg7904@gmail.com>
2023-05-22 11:53:46 +0530

    updated

--- com/Pragadesh/Main.java
+++ com/Pragadesh/Main.java
@@ -1,7 +1,30 @@
+// public class Main {
+// 	public static void main (String[] args) {
+
+// 		int x = 123; //initialization
+// 		float y = 3.14f;
+// 		boolean z = true;
+// 		char symbol = '@';
+// 		String name = "Pragadesh";
+
+// 		// System.out.println("My number is"+symbol);
+// 		System.out.println("Hello " + name);
+// 		}
+// } 
+
 public class Main {
 	public static void main (String[] args) {
-		int x = 123; //initialization
-		
-		System.out.println("My number is"+x);
+
+		String x = "water";
+		String y = "Kool-Aid";
+		String temp;
+
+		temp  = x;
+		x = y;
+		y = temp;
+
+		// System.out.println("My number is"+symbol);
+		System.out.println("x: "+x);
+		System.out.println("y: "+y);
 		}
 } 
