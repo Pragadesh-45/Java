917d4f65fc5aff6686e831bb75b844db6bd306c1
Pragadesh-45 <temporaryg7904@gmail.com>
2023-05-22 11:40:03 +0530

    updated

--- com/Pragadesh/Main.java
+++ com/Pragadesh/Main.java
@@ -1,40 +1,7 @@
-package com.Pragadesh;
-
-import java.util.Scanner;
-import java.util.Date;
-
-class Main {
-	public static void main (String args[]) {
-
-
+public class Main {
+	public static void main (String[] args) {
+		int x = 123; //initialization
 		
-
-		
-
-		
+		System.out.println("My number is"+x);
 		}
-	}
-
-
-
-		// byte age = 21;
-		// int viewsCount = 123_456_789L;
-		// float = 10.99F;
-		// char letter = 'A';
-		// boolean isEligible = false;
-		
-		// Date now = new Date();
-		// System.out.println(now);
-
-	
-
-		// //System.out.println("Hello World!");
-		// //Scanner input = new Scanner(System.in);
-		// //System.out.println(input.nextInt());
-		
-		// int rollno = 64;
-		// char letter = 'r';
-		// float marks = 98.43f;
-		// double largeDecimalNumbers = 1231243.1231;
-		// long largeInteger = 12342351;
-		// boolean check = true; //false;
+} 
