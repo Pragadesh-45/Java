4e78a4bb2a253e669ea61b9eb9d866dc454db023
Pragadesh-45 <temporaryg7904@gmail.com>
2023-05-22 12:03:24 +0530

    updated

--- com/Pragadesh/Main.java
+++ com/Pragadesh/Main.java
@@ -12,19 +12,50 @@
 // 		}
 // } 
 
+// public class Main {
+// 	public static void main (String[] args) {
+
+// 		String x = "water";
+// 		String y = "Kool-Aid";
+// 		String temp;
+
+// 		temp  = x;
+// 		x = y;
+// 		y = temp;
+
+// 		// System.out.println("My number is"+symbol);
+// 		System.out.println("x: "+x);
+// 		System.out.println("y: "+y);
+// 		}
+// } 
+
+
+import java.util.Scanner;
+
 public class Main {
 	public static void main (String[] args) {
 
-		String x = "water";
-		String y = "Kool-Aid";
-		String temp;
+		Scanner scanner = new Scanner(System.in);
 
-		temp  = x;
-		x = y;
-		y = temp;
+		System.out.println("What is ur name? ");
+		String name	= scanner.nextLine();
 
-		// System.out.println("My number is"+symbol);
-		System.out.println("x: "+x);
-		System.out.println("y: "+y);
+		System.out.println("Hello "+name);
+
+		
+		// System.out.println("x: "+x);
+		// System.out.println("y: "+y);
 		}
-} 
+}
+
+
+
+
+
+
+
+
+
+
+
+
