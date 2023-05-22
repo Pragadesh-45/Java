1308c779fb70669b733ce44be014200588b1f4b5
Pragadesh-45 <temporaryg7904@gmail.com>
2023-05-22 18:51:43 +0530

    Arrays

---
+++ com/Pragadesh/Arrays.java
@@ -0,0 +1,14 @@
+package com.Pragadesh;
+
+
+public class Arrays {
+	public static void main(String[] args) {
+		
+
+		String cars[] = {"Camaro", "Mustang", "Lancer"};
+
+		cars[0] = "Tesla";
+
+		System.out.println(cars[0]);
+	}
+}
