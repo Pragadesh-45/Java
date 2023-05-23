5b396b67db5fef5a374419bfc6ea3146866f17aa
Pragadesh-45 <temporaryg7904@gmail.com>
2023-05-23 15:56:36 +0530

    updates

--- com/Oops/Main.java
+++ com/Oops/Main.java
@@ -3,18 +3,26 @@ package com.Oops;
 public class Main {
 	public static void main(String[] args) {
 		
-		Car myCar1 = new Car();
-		Car myCar2 = new Car();
+		// Car myCar1 = new Car();
+		// Car myCar2 = new Car();
 
 
-		System.out.println(myCar1.make);
-		System.out.println(myCar1.model);
-		System.out.println();
-		System.out.println(myCar2.make);
-		System.out.println(myCar2.model);
+		// System.out.println(myCar1.make);
+		// System.out.println(myCar1.model);
+		// System.out.println();
+		// System.out.println(myCar2.make);
+		// System.out.println(myCar2.model);
 
-		// myCar.drive();
-		// myCar.brake();
+		// // myCar.drive();
+		// // myCar.brake();
+
+
+		Human human1 = new Human("Rick", 12, 40);
+		Human human2 = new Human("Morty", 13, 10);
+
+		human1.eat();
+		human2.drink();
+
 
 	}
 }
